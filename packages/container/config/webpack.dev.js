355a3f86803ec548106e1ea08dee3a3638892357
Sanjay Jain <s12jain@gmail.com>
2026-03-31 20:40:03 +0530

    created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common');
@@ -18,10 +17,7 @@ const devConfig = {
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
-        }),
-        new HTMLWebpackPlugin({
-            template: './public/index.html'
-        })
+        }),        
     ]
 };
 module.exports = merge(commonConfig, devConfig);
--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -9,9 +9,9 @@ module.exports = {
                 options: {
                     presets: ['@babel/preset-env', '@babel/preset-react'],
                     plugins: ['@babel/plugin-transform-runtime']
-                }
-            }
-        }
-    ]
-  }
+                },
+            },
+        },
+    ],
+  },  
 };  
